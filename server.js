44199cab50dcf95fab8b2d56ba0a42e7c2f05a72
EyalDelarea <eyaldelarea@gmail.com>
2020-10-02 23:44:46 +0300

    add server and build 2.10:1

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ const {exec} = require("child_process");
 const script_path = '/home/pi/RasberryPI-Light-App/projector_'
 
 
+function stingyAnswer(answer){
+
+    if(answer ==='0')
+        return 'on'
+    else
+        return 'off'
+
+}
+
+
 app.use(cors());
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, '/build')));
@@ -31,7 +41,7 @@ app.get('/off', function (req, res) {
                 return;
             }
             console.log(`stdout: ${stdout}`);
-            res.send({express: stdout});
+            res.send({express: 'off'});
 
         } catch (e) {
             console.error(e);
@@ -53,7 +63,7 @@ app.get('/on', function (req, res) {
                 return;
             }
             console.log(`stdout: ${stdout}`);
-            res.send({express: stdout});
+            res.send({express: 'on'});
 
         } catch (e) {
             console.error(e);
@@ -77,7 +87,9 @@ app.get('/get_state', function (req, res) {
                 return;
             }
             console.log(`stdout: ${stdout}`);
-            res.send({express: stdout});
+
+            answer = stingyAnswer(stdout);
+            res.send({express:answer})
 
         } catch (e) {
             console.error(e);
