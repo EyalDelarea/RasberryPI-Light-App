9c8f2275f8fa7938156f43a8ac3704c2a16ebb42
EyalDelarea <eyaldelarea@gmail.com>
2020-10-02 22:20:15 +0300

    add server and build 2.10

---
+++ build/precache-manifest.8b7d4b583f1c30f9aef60b7eec2390ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5580ae2db156e03e55d69114cf0a8868",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbaf6967c0b2b46042ca",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a49a407c41c74393e09e",
+    "url": "/static/css/main.b571202f.chunk.css"
+  },
+  {
+    "revision": "cbaf6967c0b2b46042ca",
+    "url": "/static/js/2.15cfa9b4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.15cfa9b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a49a407c41c74393e09e",
+    "url": "/static/js/main.9f959edf.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
